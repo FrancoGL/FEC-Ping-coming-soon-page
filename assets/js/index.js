1953107df1bba86772f8d11b874d80c8523d6cac
Franco Lorca <lorcafrancog@proton.me>
2023-03-24 14:05:33 -0300

    📝 Update(index.js): Add event to catch submit empty input

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,26 @@
 const $input = document.querySelector(".input");
-const $errorMessage = document.querySelector(".error__message")
+const $errorMessageOne = document.querySelector(".error__message-one")
+const $errorMessageTwo = document.querySelector(".error__message-two")
+const $btnSubmit = document.querySelector(".btn__submit");
 
 $input.addEventListener("input",(e) => {
-  console.log(e.target.validity.valid);
-  const $valid = e.target.validity.valid
-
   
-  if (!$valid) {
-    $errorMessage.classList.add("active");
+  const $valid = e.target.validity.valid
+  
+  if (!$valid && $input.value !== "") {
+    $errorMessageOne.classList.add("active");
+    $errorMessageTwo.classList.remove("active")
   }
   
   if ($valid) {
-    $errorMessage.classList.remove("active");
+    $errorMessageOne.classList.remove("active");
   }
 } )
+
+$btnSubmit.addEventListener("click",(e) => {
+  
+  if($input.value == "") {
+    $errorMessageTwo.classList.add("active");
+    $errorMessageOne.classList.remove("active");
+  }
+})
