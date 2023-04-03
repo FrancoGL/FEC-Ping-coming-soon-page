c801e18c537f3e87a84905c26b510afc4670e1a1
Franco Lorca <lorcafrancog@proton.me>
2023-04-03 15:28:37 -0300

    🚚 rename(index.js): Rename name variables

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,27 @@
-const $input = document.querySelector(".input");
-const $errorMessageOne = document.querySelector(".error__message-one")
-const $errorMessageTwo = document.querySelector(".error__message-two")
-const $btnSubmit = document.querySelector(".btn__submit");
+const input = document.querySelector(".input");
+const errorMessageOne = document.querySelector(".error__message-one")
+const errorMessageTwo = document.querySelector(".error__message-two")
+const btnSubmit = document.querySelector(".btn__submit");
 
-$input.addEventListener("input",(e) => {
+input.addEventListener("input",(e) => {
   
   const $valid = e.target.validity.valid
   
-  if (!$valid && $input.value !== "") {
-    $errorMessageOne.classList.add("active");
-    $errorMessageTwo.classList.remove("active")
+  if (!$valid && input.value !== "") {
+    errorMessageOne.classList.add("active");
+    errorMessageTwo.classList.remove("active")
   }
   
   if ($valid) {
-    $errorMessageOne.classList.remove("active");
-    $errorMessageTwo.classList.remove("active")
+    errorMessageOne.classList.remove("active");
+    errorMessageTwo.classList.remove("active")
   }
 } )
 
-$btnSubmit.addEventListener("click",(e) => {
+btnSubmit.addEventListener("click",() => {
   
-  if($input.value == "") {
-    $errorMessageTwo.classList.add("active");
-    $errorMessageOne.classList.remove("active");
+  if(input.value == "") {
+    errorMessageTwo.classList.add("active");
+    errorMessageOne.classList.remove("active");
   }
 })
